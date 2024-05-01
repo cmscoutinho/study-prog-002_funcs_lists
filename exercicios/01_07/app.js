2be179eddccc90cbf3c8fc7eb3da04ea88a39ea4
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-01 15:34:21 -0300

    01_07: solved practice challenges.

--- exercicios/01_07/app.js
+++ exercicios/01_07/app.js
@@ -1,1 +1,22 @@
-let oi;
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Hora do Desafio";
+
+function botaoConsole() {
+  console.log("O botão foi clicado.");
+}
+
+function loveJs() {
+  console.log("Eu amo JS.");
+}
+
+function botaoCidade() {
+  let cidade = prompt("Insira o nome de uma cidade brasileira.");
+  console.log(`Estive em ${cidade} e lembrei de você.`);
+}
+
+function soma() {
+  let num1 = parseInt(prompt("Digite um número."));
+  let num2 = parseInt(prompt("Digite outro número."));
+  let resultado = num1 + num2;
+  console.log(`Resultado: ${resultado}.`);
+}
