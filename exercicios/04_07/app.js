e291049088c601ec1b1869a1dbc5ced104d5517d
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-03 10:29:44 -0300

    04_07: added codes for the challenge exercises.

---
+++ exercicios/04_07/app.js
@@ -0,0 +1,20 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+listaGenerica = [];
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+linguagensDeProgramacao = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push("Java");
+linguagensDeProgramacao.push("Ruby");
+linguagensDeProgramacao.push("GoLang");
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+listaNomes = ["Putin", "Biden", "Lula"];
+console.log(listaNomes[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(listaNomes[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaNomes[listaNomes.length - 1]);
